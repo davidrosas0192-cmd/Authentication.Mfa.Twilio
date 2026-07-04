710e81628be70bf3734dbb4bf177cc033d883475
davidCruzr92 <davidcruzr92@outlook.com>
2026-07-03 20:19:19 -0700

    add properties

--- Data/Entities/ApplicationUser.cs
+++ Data/Entities/ApplicationUser.cs
@@ -2,5 +2,10 @@ namespace Authentication.Mfa.Twilio.Data.Entities;
 
 public class ApplicationUser
 {
+    public Guid Id { get; set; }
     
+    public string UserName { get; set; } = default!;
+
+
+
 }
